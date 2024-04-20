ca3454c3784a0c845236c74efa90aa90dc931644
sophea ang <angsophea@outlook.com>
2024-04-20 17:06:47 +0700

    add first name and last name

--- ConsoleApp.UserInputDemo/Program.cs
+++ ConsoleApp.UserInputDemo/Program.cs
@@ -1,18 +1,24 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
 //Declare variable
-string name = string.Empty;
+using System.Xml.Linq;
+
+string firstName = string.Empty;
+string lastName = string.Empty;
 int age = 0;
 int retirementAge = 65;
 
-Console.WriteLine("Please enter your name:");
-name = Console.ReadLine();
+Console.WriteLine("Please enter your firstname:");
+firstName = Console.ReadLine();
+
+Console.WriteLine("Please enter your lastname:");
+lastName = Console.ReadLine();
 
 Console.WriteLine("Please enter your age:");
 age = Convert.ToInt32(Console.ReadLine());
 
 int workingYearRemaining = retirementAge - age;
 
-Console.WriteLine($"Full name: {name}");
+Console.WriteLine($"Full name: {firstName} {lastName}");
 Console.WriteLine($"Age: {age}");
 Console.WriteLine($"Working years remaining: {workingYearRemaining}");
